eda744a968beb112ca27e5fca1406cc464a4fcd7
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2025-01-16 05:22:51 +0000

    le fichier de modele de movies en config

--- db/migrate/20250116044800_create_movies.rb
+++ db/migrate/20250116044800_create_movies.rb
@@ -2,12 +2,12 @@ class CreateMovies < ActiveRecord::Migration[7.1]
   def change
     create_table :movies do |t|
       t.string :title
+      t.integer :year
+      t.integer :duration
       t.text :description
       t.integer :director_id
       t.string :image_url
       t.timestamps
-      t.integer :year
-      t.integer :duration
 
     end
   end
